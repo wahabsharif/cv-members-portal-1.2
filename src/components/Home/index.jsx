efd8e6185b9554c54ce485ba8871181468f9b678
wahabsharif <www.rex19@gmail.com>
2023-09-21 15:35:21 +0500

    basics

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import Breadcrumb from "../common/Breadcrumb";
 import Footer from "../common/Footer";
 import BallotResults from "./ballot-result";
-import EmailPopup from "../Contact";
 
 const MemPortHome = () => {
   return (
     <>
       <Breadcrumb title="Members Portal" subtitle="Members Portal" />
       <BallotResults />
-      <EmailPopup />
       <Footer />
     </>
   );
